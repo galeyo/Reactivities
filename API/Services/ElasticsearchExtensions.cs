0dbc37cdd7aaad60129f2e44cba04cf148b633b7
gokhapki <galey_okhapkin@kb.cz>
2020-03-23 23:17:54 +0100

    add

---
+++ API/Services/ElasticsearchExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Domain;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Services
+{
+    public static class ElasticsearchExtensions
+    {
+        public static void AddElasticsearch(this IServiceCollection services, IConfiguration configuration)
+        {
+            var url = configuration["elasticsearch:url"];
+            var defaultIndex = configuration["elasticsearch:index"];
+
+
+            var settings = new ConnectionSettings(new Uri(url))
+                .DefaultIndex(defaultIndex)
+                .DefaultMappingFor<Activity>(m =>
+                    m.PropertyName(p => p.Id, "id")
+                );
+
+            var client = new ElasticClient(settings);
+            services.AddSingleton<IElasticClient>(client);
+
+        }
+    }
+}
